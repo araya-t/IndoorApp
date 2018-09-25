67829c9365a9b9dc74713e970a5cf3a964131b86
araya-t <araya.t@mail.kmutt.ac.th>
2018-09-25 13:15:49 +0700

    add main menu activity

---
+++ app/src/main/java/com/estimote/indoorapp/Controller/Parka/MainMenuActivity.java
@@ -0,0 +1,44 @@
+package com.estimote.indoorapp.Controller.Parka;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.estimote.indoorapp.R;
+
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnBeaconAcceCsv;
+    private Button btnReadFileSendData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        initInstances();
+
+        btnBeaconAcceCsv.setOnClickListener(this);
+        btnReadFileSendData.setOnClickListener(this);
+    }
+
+    private void initInstances() {
+        btnBeaconAcceCsv = findViewById(R.id.btnBeaconAcceCsv);
+        btnReadFileSendData = findViewById(R.id.btnReadFileSendData);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnBeaconAcceCsv){
+            String locationId = "six-slots-only--floor-10b";
+//            Intent intent = new Intent(this, CsvBeaconAcceDataActivity.class);
+            startActivity(CsvBeaconAcceDataActivity.Companion.createIntent(this,locationId));
+        }
+
+        if (v == btnReadFileSendData){
+            Intent intent = new Intent(this, ReadFileSendDataActivity.class);
+            startActivity(intent);
+        }
+    }
+}
