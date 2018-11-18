0acf57ec4506ff20d66028ec2db63c48a25178b9
araya-t <araya.t@mail.kmutt.ac.th>
2018-11-18 08:22:15 +0700

    add method for calculating (x,y) position before send value to server , calculate from value when car still to car stop

--- app/src/main/java/com/estimote/indoorapp/activity/CarDetectionActivity.java
+++ app/src/main/java/com/estimote/indoorapp/activity/CarDetectionActivity.java
@@ -40,14 +40,28 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
     private double previousAcceY = 1;
     private double previousAcceZ = 1;
     private List<CsvRow> csvRowsForCheckAcce = new ArrayList();
+//    int countIsStill = 0;
+//    int countIsStopEngine = 0;
+//    int previousCountRow = 0;
+//    int previousCountRowStill = 0;
+//    int previousCountRowStop = 0;
+//    boolean isFirstValueStill = true;
+//    boolean isFirstValueStop = true;
+//    boolean isCarStopped = false;
+
+    private List<CsvRow> csvRowsForCheckStill = new ArrayList();
+    private List<CsvRow> csvRowsForCheckStop = new ArrayList();
+    private List<CsvRow> csvRowRealSituation = new ArrayList();
     int countIsStill = 0;
     int countIsStopEngine = 0;
-    int previousCountRow = 0;
     int previousCountRowStill = 0;
     int previousCountRowStop = 0;
     boolean isFirstValueStill = true;
     boolean isFirstValueStop = true;
     boolean isCarStopped = false;
+    int indexStillStartWhenDetectStop= 0;
+    int indexStillEnd= 0;
+    int indexStop = 0;
 
 
 //    private boolean isPrintStill;
@@ -79,6 +93,16 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
 
         if (v == btnFile1) {
             String fileName = "BeaconAccelerometerData_2018-10-30_14:40:17_sampling_3microsec.csv";
+                /** pass โอเคเลย**/
+
+//            String fileName = "BeaconAccelerometerData_2018-10-30_14:42:34_sampling_3microsec.csv";
+                /** ไฟล์น่าจะเสีย **/
+
+//            String fileName = "BeaconAccelerometerData_2018-10-30_14:50:51_sampling_3microsec.csv";
+
+
+            btnFile1.setText(fileName);
+
             if (fileName != null) {
 
                 try {
@@ -223,7 +247,7 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
         // initial value of previousAcceX, previousAcceY, previousAcceZ = 1
 
         /** Check whether car is stopped or not**/
-        if (differentValueX < 0.05 && differentValueY < 0.05) {
+        if (differentValueX <= 0.06 && differentValueY <= 0.06) {
 
             Log.d("differentValue", "differentValueX: " + differentValueX + " || differentValueY: " + differentValueY);
             Log.d("CarIsStop", "-----------> Row " + countRow + " | car IS STOPPP");
@@ -242,17 +266,19 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
 
                 if (currentCountRow - previousCountRowStop == 1) {
                     countIsStopEngine++;
-                    csvRowsForCheckAcce.add(csvRow);
+                    csvRowsForCheckStop.add(csvRow);
                     previousCountRowStop = currentCountRow;
 
                     Log.d("checkIsCarStillStop", " row(" + currentCountRow
                             + ")------------- ("+ differentValueX + ", " + differentValueY + " )countIsStopEngine (" + countIsStopEngine + ") Value -------------");
 
-                } else {
-                    csvRowsForCheckAcce.clear();
+                }else if (currentCountRow - previousCountRowStop >= 3) {
+                    csvRowsForCheckStop.clear();
                     countIsStopEngine = 0;
                     previousCountRowStop = 0;
                     isFirstValueStop = true;
+                }else{
+                    csvRowsForCheckStop.add(csvRow);
                 }
 
             }
@@ -261,20 +287,48 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
                 isCarStopped = true;
                 Toast.makeText(this, "-- The car is STOP --", Toast.LENGTH_SHORT).show();
                 Log.d("checkIsCarStillStop", "--------------- The car is STOP ---------------");
+                Toast.makeText(this, "SEND DATA TO SERVER \n|| @ROW " + csvRow.getCountRow() , Toast.LENGTH_SHORT).show();
+
+                Log.d("checkIsCarStillStop", "csvRowsForCheckStill.size() : " + csvRowsForCheckStill.size()
+                                                    + "\nfrom row = " + csvRowsForCheckStill.get(0).getCountRow()
+                                                    + " - " + csvRowsForCheckStill.get(csvRowsForCheckStill.size()-1).getCountRow());
+//                indexStillStartWhenDetectStop = csvRowsForCheckStill.get(0).getCountRow();
+//                indexStillEnd = csvRowsForCheckStill.get(csvRowsForCheckStill.size()-1).getCountRow();
+
+
+                if(csvRowsForCheckStill.size() != 0){
+                    indexStillStartWhenDetectStop = csvRowsForCheckStill.get(0).getCountRow() - 1;
+                    Log.d("indexStillStartWhenStop", "indexStillStartWhenStop = " + (csvRowsForCheckStill.get(0).getCountRow() - 1) );
+
+                }else{
+                    Log.d("indexStillStartWhenStop", "--------------- csvRowsForCheckStill.size == 0 ---------------");
+                    indexStillStartWhenDetectStop = countRow - 10;
+                }
+
+                indexStop = countRow - 1;
+                Log.d("indexStillStartWhenStop", " indexStop = " + (countRow - 1) );
+
+
+                double averageXPosition = calculateAverageXPosition(indexStillStartWhenDetectStop, indexStop) + 17;
+                double averageYPosition = calculateAverageYPosition(indexStillStartWhenDetectStop, indexStop);
+
+                Log.d("sendDataToAppServer", "timestampLong1000 = " + timeStampLong/1000L
+                        + "\n, averageXPosition = " + averageXPosition
+                        + "\n, averageYPosition = " + averageYPosition + "\n fcmToken = " + fcmToken);
 
             }
 
 
 
         /** Check whether car is still or not**/
-        }else if (differentValueX < 0.15 && differentValueY < 0.15) {
+        }else if ( ((differentValueX <= 0.16) && differentValueY <= 0.16) || differentValueY <= 0.16 ) {
 
             Log.d("differentValue", "differentValueX: " + differentValueX + " || differentValueY: " + differentValueY);
             Log.d("CarIsStill", "-----------> Row " + countRow + " | car IS STILL");
 
             if (isFirstValueStill) {
                 countIsStill++;
-                csvRowsForCheckAcce.add(csvRow);
+                csvRowsForCheckStill.add(csvRow);
                 isFirstValueStill = false;
                 previousCountRowStill = currentCountRow;
 //                Log.d("checkIsCarStill", "------------- First Value -------------");
@@ -282,19 +336,22 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
 
                 Log.d("previousCountRow", "currentCountRow - previousCountRow = " + (currentCountRow - previousCountRowStill));
 
-                if (currentCountRow - previousCountRowStill <= 2) {
+                if (currentCountRow - previousCountRowStill <= 1) {
                     countIsStill++;
-                    csvRowsForCheckAcce.add(csvRow);
+                    csvRowsForCheckStill.add(csvRow);
                     previousCountRowStill = currentCountRow;
 
 //                    Log.d("checkIsCarStillStop", " row(" + currentCountRow + ")------------- countIsStill (" + countIsStill + ") Value -------------");
                     Log.d("checkIsCarStillStop", " row(" + currentCountRow
                             + ")------------- ("+ differentValueX + ", " + differentValueY + " )countIsStill (" + countIsStill + ") Value -------------");
-                } else {
-                    csvRowsForCheckAcce.clear();
+                } else if (currentCountRow - previousCountRowStill >5 ){
+                    Log.d("checkIsCarStillStop", "--------------- the || Clear csvRowsForCheckStill");
+                    csvRowsForCheckStill.clear();
                     countIsStill = 0;
                     previousCountRowStill = 0;
                     isFirstValueStill = true;
+                }else{
+                    csvRowsForCheckStill.add(csvRow);
                 }
 
             }
@@ -306,6 +363,52 @@ public class CarDetectionActivity extends AppCompatActivity implements View.OnCl
             }
         }
 
+    }
+
+    private double calculateAverageXPosition(int indexStillStartWhenDetectStop,int indexStop){
+        int i = indexStillStartWhenDetectStop;
+        int countAmount = 0;
+        double sum = 0.0;
+        double averageX = -100.0;
+
+        while ( i <= indexStop) {
+            sum += csvRows.get(i).getX_position();
+            Log.d("calculateAverage","Row " + i
+                    + "| countAmount="+ (countAmount+1)
+                    + " |x_position = " + csvRows.get(i).getX_position()
+                    + " | sum = " + sum);
+            i++;
+            countAmount++;
+        }
+
+        if(sum != 0.0){
+            averageX = sum/countAmount;
+        }
+
+        return averageX;
+    }
+
+    private double calculateAverageYPosition(int indexStillStartWhenDetectStop, int indexStop){
+        int i = indexStillStartWhenDetectStop;
+        int countAmount = 0;
+        double sum = 0.0;
+        double averageY = -100.0;
+
+        while ( i <= indexStop) {
+            sum += csvRows.get(i).getY_position();
+            Log.d("calculateAverage","Row " + i
+                    + "| countAmount="+ (countAmount + 1)
+                    + " |y_position = " + csvRows.get(i).getY_position()
+                    + " | sum = " + sum);
+            i++;
+            countAmount++;
+        }
+
+        if(sum != 0.0){
+            averageY = sum/countAmount;
+        }
+
+        return averageY;
     }
 
     private void checkIsCarEngineStopped(double differentValueX, double differentValueY, double differentValueZ
